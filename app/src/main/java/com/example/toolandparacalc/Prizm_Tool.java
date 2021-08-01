0c14102c02a33cee8de431ea06afca382928103d
AleksTereh <aleks.tereh@yandex.ru>
2021-08-01 13:37:02 +0300

    activity

---
+++ app/src/main/java/com/example/toolandparacalc/Prizm_Tool.java
@@ -0,0 +1,14 @@
+package com.example.toolandparacalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Prizm_Tool extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.prizm_tool);
+    }
+}
